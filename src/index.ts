bc405536b8429022b6885d9f786be36855ee0e08
hardzal <rizaldoeta98@gmail.com>
2026-09-09 17:51:27 +0700

    add initial file

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("Welcome to the AI Pattern project!");
+console.log("There is three study case in this project, they are: ");
+console.log("1. AI Engineer");
+console.log("2. AI Product Manager");
+console.log("3. AI Designer");
