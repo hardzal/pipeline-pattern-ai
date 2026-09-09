1981e0aa955641f0aa853bb43d8a7bec5dec59e0
hardzal <rizaldoeta98@gmail.com>
2026-09-09 17:55:24 +0700

    update index files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 console.log("Welcome to the AI Pattern project!");
 console.log("There is three study case in this project, they are: ");
-console.log("1. AI Engineer");
-console.log("2. AI Product Manager");
-console.log("3. AI Designer");
+console.log("1. Article Refiner: Evaluator-Optimizer pattern");
+console.log("2. Idea Review Board: Fan-In Fan Out pattern");
+console.log("3. Ticket Triage: Routing pattern");
